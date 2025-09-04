6f48c496d2618b0b794fe55d6bf144954a9eb56f
Andy Primawan <andyprimawan@gmail.com>
2025-09-04 22:29:01 +0700

    chore(nextjs-init): boilerplate code

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <p>Dojotek AI Chatbot is a software system to help enterprise/company/corporate to build, configure, run, monitor multiple Chatbot AI LLM RAG.</p>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <p>Dojotek AI Chatbot</p>
+      </footer>
+    </div>
+  );
+}
